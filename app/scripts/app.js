860ead4efc2e78f4bedcfce94e576c2363f97e68
Mieszko4 <mieszko4@gmail.com>
2015-11-11 16:14:40 +0000

    Install and apply angular route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,5 +8,21 @@
  *
  * Main module of the application.
  */
-angular
-  .module('foundersMapQuestApp', []);
+ angular.module('foundersMapQuestApp', [
+   'ngRoute'
+ ])
+
+ .config(function ($routeProvider) {
+   $routeProvider
+     .when('/', {
+       templateUrl: 'views/main.html',
+       controller: 'MainCtrl'
+     })
+     .when('/about', {
+       templateUrl: 'views/about.html',
+       controller: 'AboutCtrl'
+     })
+     .otherwise({
+       templateUrl: '404.html'
+     });
+ });
