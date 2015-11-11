3adbf84f07bae137c0682fdc25be56183abeda21
Mieszko4 <mieszko4@gmail.com>
2015-11-11 21:25:22 +0000

    Implement choosing lat/lng columns and form validator

--- app/scripts/controllers/load-data.js
+++ app/scripts/controllers/load-data.js
@@ -16,19 +16,32 @@ angular.module('foundersMapQuestApp')
       longitude: null
     };
 
-    $scope.ok = function () {
-      var result = Founders.parseRawData($scope.form.data, $scope.form.delimiter);
+    $scope.data = false;
+    $scope.parseRawData = function () {
+      $scope.data = Founders.parseRawData($scope.form.data, $scope.form.delimiter);
+    };
 
-      if (result !== false) {
-        $uibModalInstance.close({
-          header: result.header,
-          items: result.items,
-          latitudeColumn: $scope.form.latitude,
-          longitudeColumn: $scope.form.longitude
-        });
-      } else {
-        console.error('TODO: could not parse data');
-      }
+    $scope.formValid = false;
+    $scope.$watch(function() {
+      return $scope.data && $scope.form.latitude !== null && $scope.form.longitude !== null;
+    }, function (newValue) {
+      $scope.formValid = newValue;
+    });
+
+    $scope.columns = [];
+    $scope.$watch(function() {
+      return $scope.data;
+    }, function (newValue) {
+      $scope.columns = (newValue !== false) ? newValue.header : [];
+    }, true);
+
+    $scope.ok = function () {
+      $uibModalInstance.close({
+        header: $scope.data.header,
+        items: $scope.data.items,
+        latitudeColumn: $scope.form.latitude,
+        longitudeColumn: $scope.form.longitude
+      });
     };
 
     $scope.cancel = function () {
