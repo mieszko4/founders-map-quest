474654258b6c7ed428b2a25fded7647da2f88c7c
Mieszko4 <mieszko4@gmail.com>
2015-11-11 18:17:00 +0000

    Scaffold Founders service

---
+++ app/scripts/services/founders.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name foundersMapQuestApp.Founders
+ * @description
+ * # Founders
+ * Service in the foundersMapQuestApp.
+ */
+angular.module('foundersMapQuestApp')
+  .service('Founders', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ test/spec/services/founders.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Service: Founders', function () {
+
+  // load the service's module
+  beforeEach(module('foundersMapQuestApp'));
+
+  // instantiate service
+  var Founders;
+  beforeEach(inject(function (_Founders_) {
+    Founders = _Founders_;
+  }));
+
+  it('should do something', function () {
+    expect(!!Founders).toBe(true);
+  });
+
+});
