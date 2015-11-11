7f2e52574ba47f48f87218397be35f85056ab8e2
Mieszko4 <mieszko4@gmail.com>
2015-11-11 23:33:35 +0000

    Fix dummy test

--- test/spec/controllers/load-data.js
+++ test/spec/controllers/load-data.js
@@ -6,18 +6,29 @@ describe('Controller: LoadDataCtrl', function () {
   beforeEach(module('foundersMapQuestApp'));
 
   var LoadDataCtrl,
-    scope;
+    scope,
+    modalInstance;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
+    modalInstance = {                    // Create a mock object using spies
+      close: jasmine.createSpy('modalInstance.close'),
+      dismiss: jasmine.createSpy('modalInstance.dismiss'),
+      result: {
+        then: jasmine.createSpy('modalInstance.result.then')
+      }
+    };
+
     LoadDataCtrl = $controller('LoadDataCtrl', {
-      $scope: scope
+      $scope: scope,
+      $uibModalInstance: modalInstance,
+      state: null
       // place here mocked dependencies
     });
   }));
 
-  it('should have empty scope', function () {
+  it('should not have empty scope', function () {
     expect(Object.keys(LoadDataCtrl).length).toBe(0);
   });
 });
