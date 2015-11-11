5c93b569a798d5c14116300d7e73d024cdcfd6ce
Mieszko4 <mieszko4@gmail.com>
2015-11-11 14:12:41 +0000

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name foundersMapQuestApp
+ * @description
+ * # foundersMapQuestApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('foundersMapQuestApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name foundersMapQuestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the foundersMapQuestApp
+ */
+angular.module('foundersMapQuestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
