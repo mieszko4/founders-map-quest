1900ffab791187b3ffd5a1ceb0a72e68979b9e14
Mieszko4 <mieszko4@gmail.com>
2015-11-12 17:19:36 +0000

    Install google maps module

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,6 +28,9 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-local-storage/dist/angular-local-storage.js',
+      'bower_components/angular-simple-logger/dist/angular-simple-logger.js',
+      'bower_components/lodash/lodash.js',
+      'bower_components/angular-google-maps/dist/angular-google-maps.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
