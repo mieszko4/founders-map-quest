7fa50918d091dcff23232a3ca00baa965380ae77
Mieszko4 <mieszko4@gmail.com>
2015-11-13 00:18:30 +0000

    Visualize image and link in rows and markers

---
+++ app/scripts/filters/fmq-visualize.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name foundersMapQuestApp.filter:fmqVisualize
+ * @function
+ * @description
+ * # fmqVisualize
+ * Filter in the foundersMapQuestApp.
+ */
+angular.module('foundersMapQuestApp')
+  .filter('fmqVisualize', function ($sce) {
+    var linkRegExp = new RegExp(/https?:\/\/(.+)/);
+    var imageRegExp = new RegExp(/.+\.(jpg|jpeg|png|gif|svg|bmp)$/);
+
+    return function (input) {
+      var output = input;
+      if (linkRegExp.test(input)) {
+        if (imageRegExp.test(input)) {
+          output = '<a target="_blank" href="' + input + '"><img src="' + input + '" alt=""/></a>';
+        } else {
+          output = '<a target="_blank" href="' + input + '">' + input + '</a>';
+        }
+      } else {
+        //encode html entities
+        output = angular.element('<div />').text(input).html();
+      }
+
+      output = $sce.trustAsHtml(output);
+      return output;
+    };
+  });
---
+++ test/spec/filters/fmq-visualize.js
@@ -0,0 +1,36 @@
+'use strict';
+
+describe('Filter: fmqVisualize', function () {
+
+  // load the filter's module
+  beforeEach(module('foundersMapQuestApp'));
+
+  // initialize a new instance of the filter before each test
+  var fmqVisualize;
+  var sce;
+  beforeEach(inject(function ($filter, $sce) {
+    fmqVisualize = $filter('fmqVisualize');
+    sce = $sce;
+  }));
+
+  it('should pass through name', function () {
+    var input = 'Miłosz';
+
+    expect(sce.getTrustedHtml(fmqVisualize(input))).toEqual(input);
+  });
+
+  it('should visualize link', function () {
+    var input = 'http://milosz.ch/';
+    var output = '<a target="_blank" href="' + input + '">' + input + '</a>';
+
+    expect(sce.getTrustedHtml(fmqVisualize(input))).toEqual(output);
+  });
+
+  it('should visualize image', function () {
+    var input = 'https://milosz.ch/ja-dran.s.jpg';
+    var output = '<a target="_blank" href="' + input + '"><img src="' + input + '" alt=""/></a>';
+
+    expect(sce.getTrustedHtml(fmqVisualize(input))).toEqual(output);
+  });
+
+});
