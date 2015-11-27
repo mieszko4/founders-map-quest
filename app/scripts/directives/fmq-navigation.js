7be0fec4e22a5c071f1ef0113b4c61ba2729305b
Milosz Chmura <mieszko4@gmail.com>
2015-11-27 14:17:52 +0000

    Fix navigation directive

--- app/scripts/directives/fmq-navigation.js
+++ app/scripts/directives/fmq-navigation.js
@@ -7,24 +7,23 @@
  * # fmqNavigation
  */
 angular.module('foundersMapQuestApp')
-  .directive('fmqNavigation', function () {
+  .directive('fmqNavigation', function ($state, FMQ_ROOT_URL) {
     return {
       templateUrl: 'views/directives/fmq-navigation.html',
       restrict: 'A',
       replace: true,
       link: function (scope) {
-        /*
-        scope.route = $route;
-        scope.items = [
-          {
-            path: '/',
-            label: 'Home'
-          },
-          {
-            path: '/about',
-            label: 'About'
-          }
-        ];*/
+        scope.$state = $state;
+
+        scope.items = scope.$state.get()
+          .filter(function (state) { //filter urls of module paths
+            return !state.abstract && typeof state.url !== 'undefined' && state.url !== FMQ_ROOT_URL;
+          }).map(function (state) { //create label based on last part of state name
+            return {
+              name: state.name,
+              label: state.name.split('.').pop() //
+            };
+          });
       }
     };
   });
