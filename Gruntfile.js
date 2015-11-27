4af1365a0e00592134754b566281aa266e5dc76e
Milosz Chmura <mieszko4@gmail.com>
2015-11-27 18:50:56 +0000

    Adjust gruntfile for views

--- Gruntfile.js
+++ Gruntfile.js
@@ -373,7 +373,7 @@ module.exports = function (grunt) {
           usemin: 'scripts/scripts.js'
         },
         cwd: '<%= yeoman.app %>',
-        src: 'views/{,*/}*.html',
+        src: 'components/**/*.html',
         dest: '.tmp/templateCache.js'
       }
     },
