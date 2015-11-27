2f48685e3643da5255bb388c2353edcb3bcd109a
Milosz Chmura <mieszko4@gmail.com>
2015-11-27 15:02:07 +0000

    Fix test watcher

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,6 @@
 // Generated on 2015-11-11 using generator-angular 0.14.0
 'use strict';
 
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
-
 module.exports = function (grunt) {
 
   // Time how long tasks take. Can help when optimizing build times
@@ -24,6 +18,9 @@ module.exports = function (grunt) {
     app: require('./bower.json').appPath || 'app',
     dist: 'dist'
   };
+
+  // Unit test matcher
+  var unitTestRegExp = '<%= yeoman.app %>/components/**/*.spec.js';
 
   // Define the configuration for all the tasks
   grunt.initConfig({
@@ -45,7 +42,7 @@ module.exports = function (grunt) {
         }
       },
       jsTest: {
-        files: ['test/spec/{,*/}*.js'],
+        files: [unitTestRegExp],
         tasks: ['newer:jshint:test', 'newer:jscs:test', 'karma']
       },
       compass: {
@@ -134,7 +131,7 @@ module.exports = function (grunt) {
         options: {
           jshintrc: 'test/.jshintrc'
         },
-        src: ['test/spec/{,*/}*.js']
+        src: [unitTestRegExp]
       }
     },
 
@@ -151,7 +148,7 @@ module.exports = function (grunt) {
         ]
       },
       test: {
-        src: ['test/spec/{,*/}*.js']
+        src: [unitTestRegExp]
       }
     },
 
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,8 +35,8 @@ module.exports = function(config) {
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
-      "test/mock/**/*.js",
-      "test/spec/**/*.js"
+      "app/components/**/*.js",
+      "test/mock/**/*.js"
     ],
 
     // list of files / patterns to exclude
