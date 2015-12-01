28de2a39ef315df29c731a89136c7bdb0fb547c2
Milosz Chmura <mieszko4@gmail.com>
2015-12-01 06:14:14 +0000

    Add test for column-sorter directive

--- app/components/column-sorter/fmq-column-sorter.directive.spec.js
+++ app/components/column-sorter/fmq-column-sorter.directive.spec.js
@@ -3,18 +3,54 @@
 describe('Directive: fmqColumnSorter', function () {
 
   // load the directive's module
+  beforeEach(module('templates'));
   beforeEach(module('foundersMapQuestApp.columnSorter'));
 
   var element,
-    scope;
+    callbacks,
+    state,
+    $scope;
 
   beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
+    callbacks = {
+      change: function () {}
+    };
+
+    spyOn(callbacks, 'change');
+    $scope = $rootScope.$new();
+    $scope.state = state;
+
+    $scope.changeCallback = callbacks.change;
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<fmq-column-sorter></fmq-column-sorter>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('');
+  it('should make hidden element visible', inject(function ($compile, SortStates) {
+    element = angular.element('<fmq-column-sorter state="state" change="changeCallback(state)"></fmq-column-sorter>');
+    element = $compile(element)($scope);
+    $scope.$digest();
+
+    expect(element.find('span').length).toBe(3);
+    expect(element.isolateScope().vm.state).toBe(SortStates.NONE);
+    expect(element.find('span.ng-hide').length).toBe(2); //only one visible at the time
+
+    element.trigger('click');
+    $scope.$digest();
+
+    expect(callbacks.change).toHaveBeenCalledWith(SortStates.ASC);
+    expect(element.isolateScope().vm.state).toBe(SortStates.ASC);
+    expect(element.find('span.ng-hide').length).toBe(2);
+
+    element.trigger('click');
+    $scope.$digest();
+
+    expect(callbacks.change).toHaveBeenCalledWith(SortStates.DESC);
+    expect(element.isolateScope().vm.state).toBe(SortStates.DESC);
+    expect(element.find('span.ng-hide').length).toBe(2);
+
+    element.trigger('click');
+    $scope.$digest();
+
+    expect(callbacks.change).toHaveBeenCalledWith(SortStates.NONE);
+    expect(element.isolateScope().vm.state).toBe(SortStates.NONE);
+    expect(element.find('span.ng-hide').length).toBe(2);
   }));
 });
