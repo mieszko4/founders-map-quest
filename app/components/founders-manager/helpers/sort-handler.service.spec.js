eaf4e3c48f5be2869ead571d53d6bbb8bc5abf65
Milosz Chmura <mieszko4@gmail.com>
2015-12-04 14:45:34 +0000

    Add tests for sort handler

--- app/components/founders-manager/helpers/sort-handler.service.spec.js
+++ app/components/founders-manager/helpers/sort-handler.service.spec.js
@@ -11,8 +11,39 @@ describe('Service: SortHandler', function () {
     SortHandler = _SortHandler_;
   }));
 
-  it('should do something', function () {
+  it('should exist', function () {
     expect(!!SortHandler).toBe(true);
   });
 
+  it('should return state', function () {
+    var SortStates = {0: 'state1'};
+
+    expect(SortHandler.getSortState(SortStates, 0)).toBe('state1');
+    expect(SortHandler.getSortState(SortStates, 'non-existing')).not.toBeDefined();
+  });
+
+  it('should return sort keys', function () {
+    var SortStates = {0: 'state1', 1: 'state2'};
+
+    expect(SortHandler.getSortKeys(SortStates)).toEqual(['0', '1']);
+  });
+
+  it('should return apply sort', function () {
+    var SortStates = {0: 'state1', 1: 'state2'};
+
+    expect(SortHandler.applySort(SortStates, 'state1', 1)).toEqual({
+      0: 'state1',
+      1: 'state1'
+    });
+
+    expect(SortHandler.applySort(SortStates, 'state3', 'non-existing')).toEqual({
+      0: 'state1',
+      1: 'state1',
+      'non-existing': 'state3'
+    });
+  });
+
+  it('should reset sort', function () {
+    expect(SortHandler.resetSorts()).toEqual({});
+  });
 });
