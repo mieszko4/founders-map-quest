c0b69201127e93482fa89e7771ba143e9388d0cf
Milosz Chmura <mieszko4@gmail.com>
2015-12-04 00:26:57 +0000

    Add tests for decimal2dms

--- app/components/dashboard/visualize/decimal2dms.filter.spec.js
+++ app/components/dashboard/visualize/decimal2dms.filter.spec.js
@@ -11,8 +11,58 @@ describe('Filter: decimal2dms', function () {
     decimal2dms = $filter('decimal2dms');
   }));
 
-  it('should return the input prefixed with "decimal2dms filter:"', function () {
+  it('should transform for positive latitude', function () {
     expect(decimal2dms(55.751667, true)).toBe('55° 45′ 06″ N');
+  });
+
+  it('should transform for positive longitude', function () {
+    expect(decimal2dms(55.751667, false)).toBe('55° 45′ 06″ E');
+  });
+
+  it('should transform for negative latitude', function () {
+    expect(decimal2dms(-55.751667, true)).toBe('55° 45′ 06″ S');
+  });
+
+  it('should transform for negative longitude', function () {
+    expect(decimal2dms(-55.751667, false)).toBe('55° 45′ 06″ W');
+  });
+
+  it('should transform without padding', function () {
+    expect(decimal2dms(10.99, false)).toBe('10° 59′ 24″ E');
+  });
+
+  it('should transform with padding', function () {
+    expect(decimal2dms(10.084, false)).toBe('10° 05′ 02″ E');
+  });
+
+  //edge cases
+  it('should transform for 0 latitude', function () {
+    expect(decimal2dms(0, true)).toBe('0° 00′ 00″ N');
+  });
+
+  it('should transform for 0 longitude', function () {
+    expect(decimal2dms(0, false)).toBe('0° 00′ 00″ E');
+  });
+
+  it('should transform for 180 latitude', function () {
+    expect(decimal2dms(180, true)).toBe('180° 00′ 00″ N');
+  });
+
+  it('should transform for 180 longitude', function () {
+    expect(decimal2dms(180, false)).toBe('180° 00′ 00″ E');
+  });
+
+  it('should transform for -180 latitude', function () {
+    expect(decimal2dms(-180, true)).toBe('180° 00′ 00″ S');
+  });
+
+  it('should transform for -180 longitude', function () {
+    expect(decimal2dms(-180, false)).toBe('180° 00′ 00″ W');
+  });
+
+  it('should return empty for exceeding or not number', function () {
+    expect(decimal2dms(200, false)).toBe('');
+    expect(decimal2dms('string', false)).toBe('');
   });
 
 });
