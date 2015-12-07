4489833c697cd9853619081e0065b65bc962640e
Milosz Chmura <mieszko4@gmail.com>
2015-12-07 11:23:18 +0000

    Add e2e test for displaying table and map

--- test/app.e2e.js
+++ test/app.e2e.js
@@ -1,4 +1,5 @@
 'use strict';
+var path = require('path');
 
 describe('foundersMapQuestApp typical usage', function() {
   it('should automatically redirect to /dasboard when location hash/fragment is empty', function() {
@@ -75,6 +76,20 @@ describe('foundersMapQuestApp typical usage', function() {
         expect(browser.getLocationAbsUrl()).toMatch('/about');
       });
     });
+
+    it('should not go to show-image directly', function () {
+      //go to not-found page
+      browser.get('index.html#/non-existing');
+      browser.get('index.html#/dashboard/show-image');
+      expect(browser.getLocationAbsUrl()).toMatch('/dashboard');
+    });
+
+    it('should not go to map when it is not loaded', function () {
+      //go to not-found page
+      browser.get('index.html#/non-existing');
+      browser.get('index.html#/dashboard/map');
+      expect(browser.getLocationAbsUrl()).toMatch('/dashboard');
+    });
   });
 
   describe('should show dashboard with load button only', function () {
@@ -102,7 +117,8 @@ describe('foundersMapQuestApp typical usage', function() {
   });
 
   describe('should show load data dialog with empty data', function () {
-    var loadButton;
+    var loadButton,
+      loadData;
     beforeAll(function () {
       browser.get('index.html#/dashboard');
       loadButton = element(by.css('.fmq-dashboard-page .btn-load-data'));
@@ -111,6 +127,188 @@ describe('foundersMapQuestApp typical usage', function() {
     it('should open load data dialog', function () {
       loadButton.click();
       expect(browser.getLocationAbsUrl()).toMatch('/load-data');
+
+      loadData = element(by.css('.fmq-load-data-page'));
+      expect(loadData.isDisplayed()).toBe(true);
+    });
+
+    it('should have empty form', function () {
+      var raw =  element(by.model('vm.form.raw'));
+      var delimiter = element.all(by.model('vm.form.delimiter')).filter(function (input) {
+        return input.isSelected();
+      });
+      var latitudeColumn =  element(by.model('vm.form.latitudeColumn'));
+      var longitudeColumn =  element(by.model('vm.form.longitudeColumn'));
+
+      expect(raw.getAttribute('value')).toBe('');
+      expect(delimiter.getAttribute('value')).toEqual([',']);
+      expect(latitudeColumn.getAttribute('value')).toBe('');
+      expect(longitudeColumn.getAttribute('value')).toBe('');
+    });
+  });
+
+  describe('should load data from file', function () {
+    var chooseFile,
+      modalFooter;
+
+    beforeAll(function () {
+      browser.get('index.html#/non-existing');
+      browser.get('index.html#/dashboard/load-data');
+      expect(browser.getLocationAbsUrl()).toMatch('/load-data');
+
+      chooseFile = element(by.css('.fmq-load-data-page input[type=file]'));
+      modalFooter = element(by.css('.modal-footer'));
+    });
+
+    it('should have choose file button', function () {
+      chooseFile.element(by.xpath('..')).getText().then(function (text) {
+        expect(text.toLowerCase()).toMatch('choose file');
+      });
+    });
+
+    it('should parse data taken from file', function () {
+      var fileToUpload = 'fixtures/sample.colon.csv',
+      absolutePath = path.resolve(__dirname, fileToUpload);
+
+      chooseFile.sendKeys(absolutePath);
+
+      var raw =  element(by.model('vm.form.raw'));
+      var delimiter =  element.all(by.model('vm.form.delimiter')).filter(function (input) {
+        return input.isSelected();
+      });
+      var latitudeColumn =  element(by.model('vm.form.latitudeColumn'));
+      var longitudeColumn =  element(by.model('vm.form.longitudeColumn'));
+
+      expect(raw.getAttribute('value')).not.toBe('');
+      expect(delimiter.getAttribute('value')).toEqual([';']);
+      expect(latitudeColumn.getAttribute('value')).toBe('number:9');
+      expect(longitudeColumn.getAttribute('value')).toBe('number:10');
+    });
+
+    it('should load data', function () {
+      modalFooter.all(by.css('button')).filter(function (button) {
+        return button.getText().then(function (text) {
+          return text.toLowerCase().match('load');
+        });
+      }).then(function (filteredButtons) {
+        expect(filteredButtons.length).toBe(1);
+        filteredButtons[0].click();
+        expect(browser.getLocationAbsUrl()).toMatch('/dashboard');
+      });
+    });
+  });
+
+  describe('should display table and map', function () {
+    var map,
+      table;
+
+    beforeAll(function () {
+      map = element(by.css('.fmq-dashboard-page .map-container'));
+      table = element(by.css('.fmq-dashboard-page .table-container'));
+    });
+
+    it('should have elements present', function () {
+      expect(map.isDisplayed()).toBe(true);
+      expect(table.isDisplayed()).toBe(true);
+    });
+
+    it('should filter by text', function () {
+      //TODO
+    });
+
+    it('should filter by selection', function () {
+      //TODO
+    });
+
+    it('should select marker', function () {
+      //TODO
+    });
+
+    it('should sort', function () {
+      //TODO
+    });
+
+    it('should show image', function () {
+      //TODO
+    });
+
+    it('should go to map and open marker', function () {
+      //TODO
+    });
+  });
+
+  describe('should show load data with empty form when going directly and data is loaded', function () {
+    var modalFooter;
+
+    beforeAll(function () {
+      browser.get('index.html#/non-existing');
+      browser.get('index.html#/dashboard/load-data');
+      expect(browser.getLocationAbsUrl()).toMatch('/load-data');
+
+      modalFooter = element(by.css('.modal-footer'));
+    });
+
+    it('should have empty form', function () {
+      var raw =  element(by.model('vm.form.raw'));
+      var delimiter = element.all(by.model('vm.form.delimiter')).filter(function (input) {
+        return input.isSelected();
+      });
+      var latitudeColumn =  element(by.model('vm.form.latitudeColumn'));
+      var longitudeColumn =  element(by.model('vm.form.longitudeColumn'));
+
+      expect(raw.getAttribute('value')).toBe('');
+      expect(delimiter.getAttribute('value')).toEqual([',']);
+      expect(latitudeColumn.getAttribute('value')).toBe('');
+      expect(longitudeColumn.getAttribute('value')).toBe('');
+    });
+
+    it('should go back to dashboard on cancel', function () {
+      modalFooter.all(by.css('button')).filter(function (button) {
+        return button.getText().then(function (text) {
+          return text.toLowerCase().match('cancel');
+        });
+      }).then(function (filteredButtons) {
+        expect(filteredButtons.length).toBe(1);
+        filteredButtons[0].click();
+        expect(browser.getLocationAbsUrl()).toMatch('/dashboard');
+      });
+    });
+  });
+
+  describe('should show load data with non-empty form when going from dashboard', function () {
+    var loadButton;
+
+    beforeAll(function () {
+      browser.get('index.html#/dashboard');
+      expect(browser.getLocationAbsUrl()).toMatch('/dashboard');
+
+      loadButton = element(by.css('.fmq-dashboard-page .btn-load-data'));
+    });
+
+    it('should have load again button', function () {
+      expect(loadButton.isPresent()).toBe(true);
+      loadButton.getText().then(function (text) {
+        expect(text.toLowerCase()).toMatch('again');
+      });
+    });
+
+    it('should navigate to load-data', function () {
+      loadButton.click();
+      expect(browser.getLocationAbsUrl()).toMatch('/load-data');
+    });
+
+    it('should not have empty form', function () {
+      var raw =  element(by.model('vm.form.raw'));
+      var delimiter =  element.all(by.model('vm.form.delimiter')).filter(function (input) {
+        return input.isSelected();
+      });
+      var latitudeColumn =  element(by.model('vm.form.latitudeColumn'));
+      var longitudeColumn =  element(by.model('vm.form.longitudeColumn'));
+
+      expect(raw.getAttribute('value')).not.toBe('');
+      expect(delimiter.getAttribute('value')).toEqual([';']);
+      expect(latitudeColumn.getAttribute('value')).toBe('number:9');
+      expect(longitudeColumn.getAttribute('value')).toBe('number:10');
     });
   });
 });
